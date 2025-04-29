5005f393ddebf2288e2a3940471f1af0a2d92a2f
ValmirJrDS <valmirmoreirajunior@gmail.com>
2025-04-29 19:53:31 -0300

    função adcionada

--- tests/test_funcao_ola.py
+++ tests/test_funcao_ola.py
@@ -1,1 +1,6 @@
-from app.funcao import funcao_ola
+from app.funcao import funcao_ola_turma
+
+def test_funcao_ola_turma_retorna_ola_jornda():
+    saida = funcao_ola_turma()
+    gabarito = 'ola_jornada'
+    assert saida == gabarito
